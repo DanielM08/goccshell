9f615d3550715f9d5941f045e264a86e6ac71c2a
Daniel Carvalho <daniel.carvalho@maxmilhas.com.br>
2024-01-13 19:53:05 -0300

    feat: adding support to pipes

--- main.go
+++ main.go
@@ -20,47 +20,131 @@ func main() {
 			continue
 		}
 
-		parts := strings.Fields(input)
-		var command = parts[0]
-		var args = parts[1:]
+		commandsList := strings.Split(input, "|")
 
-		if command == "exit" {
-			os.Exit(0)
-		}
-
-		if command == "cd" {
-			if len(args) == 0 {
-				continue
-			}
-
-			if args[0] == "~" {
-				dir, _ := os.UserHomeDir()
-				err := os.Chdir(dir)
-
-				if err != nil {
-					fmt.Printf("%v\n", err)
-				}
-
-				continue
-			}
-
-			err := os.Chdir(args[0])
+		var commandsExecList []*exec.Cmd = []*exec.Cmd{}
+		var err error
+		for _, commandInput := range commandsList {
+			err, command := executeCommand(commandInput)
 
 			if err != nil {
-				fmt.Printf("%v\n", err)
+				break
 			}
 
+			if command == nil {
+				break
+			}
+
+			commandsExecList = append(commandsExecList, command)
+		}
+
+		if err != nil {
+			fmt.Printf("%v\n", err)
 			continue
 		}
 
-		cmd := exec.Command(command, args...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+		if len(commandsExecList) == 1 {
+			cmd := commandsExecList[0]
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
 
-		err := cmd.Run()
+			err = cmd.Run()
+
+		} else {
+			executePipedCommands(commandsExecList)
+		}
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("%v\n", err)
 		}
 	}
+}
+
+func executeCommand(input string) (error, *exec.Cmd) {
+	parts := strings.Fields(input)
+
+	var err error
+
+	var command = parts[0]
+	var args = parts[1:]
+
+	if command == "exit" {
+		os.Exit(0)
+	}
+
+	if command == "cd" {
+		if len(args) == 0 {
+			return nil, nil
+		}
+
+		if args[0] == "~" {
+			dir, _ := os.UserHomeDir()
+			err = os.Chdir(dir)
+
+			if err != nil {
+				return nil, nil
+			}
+
+			return nil, nil
+		}
+
+		err := os.Chdir(args[0])
+
+		if err != nil {
+			return err, nil
+		}
+
+		return nil, nil
+	}
+
+	cmd := exec.Command(command, args...)
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+
+	// err = cmd.Run()
+
+	// if err != nil {
+	// 	return err, nil
+	// }
+
+	return nil, cmd
+}
+
+func executePipedCommands(commandsExecList []*exec.Cmd) error {
+
+	for i, cmd := range commandsExecList {
+		if i > 0 {
+			prevCmd := commandsExecList[i-1]
+			stdout, err := prevCmd.StdoutPipe()
+
+			if err != nil {
+				return err
+			}
+
+			cmd.Stdin = stdout
+		}
+
+		if i == len(commandsExecList)-1 {
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+		}
+	}
+
+	for _, cmd := range commandsExecList {
+		err := cmd.Start()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, cmd := range commandsExecList {
+		err := cmd.Wait()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
